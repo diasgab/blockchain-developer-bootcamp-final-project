5b478b8da55234d6a2d61f10a89632b1c411ed1a
Gabriel Dias <diasgab@gmail.com>
2021-11-28 20:19:50 +0700

    Remove Buyer contract and replace it by a JS script

--- migrations/3_buyer_migration.js
+++
@@ -1,5 +0,0 @@
-const Buyer = artifacts.require("Buyer");
-
-module.exports = function (deployer) {
-  deployer.deploy(Buyer);
-};
--- scripts/buyer_simulator.js
+++ scripts/buyer_simulator.js
@@ -1,29 +1,140 @@
 
-const Buyer = artifacts.require('Buyer')
-const UNI_ADDRESS = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984'
+module.exports = async (callback) => {
 
-module.exports = async callback => {
   try {
 
-    const buyer = await Buyer.deployed();
     const accounts = await web3.eth.getAccounts();
+    const account = accounts[0];
+    const recipient = accounts[0];
 
-    console.log("Let's change the price of UNI token using the Buyer contract");
+    // transfer a big amount to the first account
+    let totalAmountToBuy = 0
+    for (let i = 4; i < accounts.length; i++) {
+      console.log("Sending 90 ether to buyer account: " + account);
+      await web3.eth.sendTransaction({
+        from: accounts[i],
+        to: account,
+        value: web3.utils.toWei("90", "ether")
+      })
 
-    await buyer.deposit({from: accounts[2], value: 90000000000000000000})
-    await buyer.deposit({from: accounts[3], value: 90000000000000000000})
-    await buyer.deposit({from: accounts[4], value: 90000000000000000000})
-    await buyer.deposit({from: accounts[5], value: 90000000000000000000})
-    await buyer.deposit({from: accounts[6], value: 90000000000000000000})
+      totalAmountToBuy += 90;
+    }
 
-    await buyer.swapETHForTokens(UNI_ADDRESS, web3.utils.toWei('450', 'ether'))
+    const addresses = {
+      WETH: "0xc778417E063141139Fce010982780140Aa0cD5Ab",
+      UNI: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
+      router: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+    };
 
-    console.log("Bought 450 ETH of UNI Token");
+    const jsonAbi = {
+      "abi": [
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "amountIn",
+              "type": "uint256"
+            },
+            {
+              "internalType": "address[]",
+              "name": "path",
+              "type": "address[]"
+            }
+          ],
+          "name": "getAmountsOut",
+          "outputs": [
+            {
+              "internalType": "uint256[]",
+              "name": "amounts",
+              "type": "uint256[]"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "amountOutMin",
+              "type": "uint256"
+            },
+            {
+              "internalType": "address[]",
+              "name": "path",
+              "type": "address[]"
+            },
+            {
+              "internalType": "address",
+              "name": "to",
+              "type": "address"
+            },
+            {
+              "internalType": "uint256",
+              "name": "deadline",
+              "type": "uint256"
+            }
+          ],
+          "name": "swapExactETHForTokens",
+          "outputs": [
+            {
+              "internalType": "uint256[]",
+              "name": "amounts",
+              "type": "uint256[]"
+            }
+          ],
+          "stateMutability": "payable",
+          "type": "function"
+        },
+      ],
+    };
 
-    callback()
+    web3.eth.defaultAccount = account;
+    const router = new web3.eth.Contract(
+      jsonAbi.abi,
+      addresses.router
+    );
+
+    console.log(
+      "Let's change the price of UNI token by buying a huge quantity"
+    );
+
+    let tokenIn = addresses.WETH;
+    let tokenOut = addresses.UNI;
+
+    const amountIn = web3.utils.toWei(String(totalAmountToBuy), "ether");
+
+    const amounts = await router.methods.getAmountsOut(amountIn, [tokenIn, tokenOut]).call();
+    const amountOutMin = web3.utils.toWei(web3.utils.fromWei(amounts[1]), "ether");
+
+    console.log(`
+      Buying new token
+      =================
+      tokenIn: ${amountIn.toString()} ${tokenIn} (WETH)
+      tokenOut: ${amountOutMin.toString()} ${tokenOut}
+    `);
+
+    const nowInSeconds = Math.floor(Date.now() / 1000);
+    const expiryDate = nowInSeconds + 900;
+
+    const tx = await router.methods.swapExactETHForTokens(
+        amountOutMin,
+        [tokenIn, tokenOut],
+        recipient,
+        expiryDate
+      ).send({
+        from: account,
+        gasLimit: 1000000,
+        gasPrice: web3.utils.toWei('10', 'Gwei'),
+        value: amountIn
+      });
+
+      console.log("Transaction made");
+      console.log(tx)
+
+    callback();
   } catch (err) {
     console.log("Error: ", err);
-    callback()
+    callback();
   }
-}
-
+};
